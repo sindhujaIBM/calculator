b3a87ffb718d21de8f40a9ac08743ffa7cc96ac8
Sindhuja Kali Srinivasan <sindhuja.kalisrinivasan@paysafe.com>
2021-03-18 23:07:55 -0600

    Auto-generated commit

--- env.js
+++ env.js
@@ -2,5 +2,5 @@
   window.__env = window.__env || {};
 
   // API url
-  window.__env.apiUrl = '/calculator/assets/data/api/';
+  window.__env.apiUrl = 'https://sindhujaibm.github.io/calculator/assets/data/api/';
 }(this));
